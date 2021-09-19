477e2f565a8c9105ebda2ad7e42cb0f003925b31
Ramin Sharifi <raminsharifi@w134-87-162-050.wireless.uvic.ca>
2021-09-19 15:37:16 -0700

    general fixes applied

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import argparse
 from pointToLine import point_to_line
 import os
+import bcolors
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-a', '--point_a', nargs='+', type=float,
-                    help='Should be numerical values of type int / float.')
-parser.add_argument('-b', '--point_b', nargs='+', type=float,
-                    help='Should be numerical values of type int / float.')
+parser.add_argument('-a', '--point_a', nargs=2, type=float,
+                    help='Should be numerical values of type int / float.'
+                         ' Seperated by space.')
+parser.add_argument('-b', '--point_b', nargs=2, type=float,
+                    help='Should be numerical values of type int / float. '
+                         ' Seperated by space')
 parser.add_argument('-d', '--decimal_points', default=3, type=int,
                     help='Specifies the number of decimal points in output. '
                          'default value is 3.')
@@ -20,16 +23,25 @@ if __name__ == '__main__':
     point_b = args.point_b
     decimal_point = args.decimal_points
 
-    output = point_to_line(point_a, point_b, decimal_point)
+    output, Error = point_to_line(point_a, point_b, decimal_point)
+
+    if output is not None:
+        print(f"{bcolors.BOLD}*********** OUTPUT ***********{bcolors.ENDC}")
+        print(f"{bcolors.BITALIC}{output}{bcolors.ENDC}")
+
     if args.save_to_file:
+        print("*********** Saving output to CSV file ***********")
         if os.path.isfile('point_to_file.csv'):
             with open('point_to_file.csv', 'a') as f:
                 f.write(
                     f"\n{point_a[0]},{point_a[1]},{point_b[0]}, {point_b[1]}, "
-                    f"{decimal_point}, {output}")
+                    f"{decimal_point}, {output}, {Error}")
         else:
             with open('point_to_file.csv', 'w') as f:
-                f.write("X1, Y1, X2, Y2, decimal_point, line_output\n")
+                f.write("X1, Y1, X2, Y2, decimal_point, line_output, Error\n")
                 f.write(
                     f"{point_a[0]},{point_a[1]},{point_b[0]}, {point_b[1]}, "
-                    f"{decimal_point}, {output}")
+                    f"{decimal_point}, {output}, {Error}")
+        print(" Results saved to 'point_to_file.csv'")
+
+    print(f"{bcolors.OKMSG}*********** Done! ***********{bcolors.ENDC}")
--- pointToLine.py
+++ pointToLine.py
@@ -1,13 +1,17 @@
 import math
+import bcolors
 
 
-def point_to_line(point_a: list, point_b: list, decimal_point=3) -> str:
+def point_to_line(point_a: list, point_b: list, decimal_point=3) -> (str, str):
     try:
         assert isinstance(point_a, list) and len(point_a) == 2
         assert isinstance(point_b, list) and len(point_b) == 2
-    except AssertionError:
-        print("Could Not verify your inputs are of type list, or length two")
-        return "No Answer, Error raised"
+        assert isinstance(decimal_point, int)
+    except AssertionError as err:
+        print(
+            f"{bcolors.FAIL}*********** Error Detected ***********{bcolors.ENDC}")
+        print("Input types Error!")
+        return None, err
     try:
         var_a = point_a[1] - point_b[1]
         var_b = point_b[0] - point_a[0]
@@ -15,21 +19,30 @@ def point_to_line(point_a: list, point_b: list, decimal_point=3) -> str:
         assert math.isinf(var_a) is not True and math.isinf(var_b) is not True
         if var_a == 0 and var_b == 0:
             raise ValueError(
-                "Both points were equal, Please provide two different points"
+                "Both points were equal. Please provide two different points"
             )
 
     except TypeError as err:
+        print(
+            f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}"
+        )
         print("Make sure your inputs are numerical!")
         print(f"Your error is: {err}.")
-        return "No Answer, Error raised"
+        return None, err
     except AssertionError as err:
+        print(
+            f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}"
+        )
         print("Yours inputs for should be less than Infinity!")
         print(f"Your error is: {err}!")
-        return "No Answer, Error raised"
+        return None, err
     except ValueError as err:
+        print(
+            f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}"
+        )
         print("Please input two different points!")
         print(f"Your error is: {err}!")
-        return "No Answer, Error raised"
+        return None, err
 
     else:
         if var_a == 0:
@@ -43,4 +56,4 @@ def point_to_line(point_a: list, point_b: list, decimal_point=3) -> str:
                       f"+ ({var_b:.{decimal_point}f}) * Y " \
                       f"= {var_c:.{decimal_point}f}"
 
-        return outline
+        return outline, None
