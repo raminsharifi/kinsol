06c9cee4d0871e0e9c0137ba329d4aa5dff8e4b5
Ramin Sharifi <raminsharifi@w134-87-162-050.wireless.uvic.ca>
2021-09-19 17:34:09 -0700

    reformatted code

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import argparse
-from pointToLine import point_to_line
 import os
 import bcolors
+from point_to_line import point_to_line
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-a', '--point_a', nargs=2, type=float,
-                    help='Should be numerical two values of type int / float.'
-                         ' Seperated by space.')
-parser.add_argument('-b', '--point_b', nargs=2, type=float,
-                    help='Should be numerical two values of type int / float. '
-                         ' Seperated by space')
-parser.add_argument('-d', '--decimal_points', default=3, type=int,
-                    help='Specifies the number of decimal points in output. '
-                         'Should be greater or equal to zero.'
-                         'default value is 3.')
-parser.add_argument('-s', '--save_to_file', default=True, type=bool,
-                    help='accpets: Boolean, if True: '
-                         'Will write to csv file')
+parser = argparse.ArgumentParser(description="Process some integers.")
+parser.add_argument(
+    "-a",
+    "--point_a",
+    nargs=2,
+    type=float,
+    help="Should be numerical two values of type int / float. Seperated by space.",
+)
+parser.add_argument(
+    "-b",
+    "--point_b",
+    nargs=2,
+    type=float,
+    help="Should be numerical two values of type int / float. Seperated by space",
+)
+parser.add_argument(
+    "-d",
+    "--decimal_points",
+    default=3,
+    type=int,
+    help="Specifies the number of decimal points in output. "
+    "Should be greater or equal to zero."
+    "default value is 3.",
+)
+parser.add_argument(
+    "-s",
+    "--save_to_file",
+    default=True,
+    type=bool,
+    help="accpets: Boolean, if True: " "Will write to csv file",
+)
 args = parser.parse_args()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     point_a = args.point_a
     point_b = args.point_b
     decimal_point = args.decimal_points
@@ -32,17 +49,19 @@ if __name__ == '__main__':
 
     if args.save_to_file:
         print("*********** Saving output to CSV file ***********")
-        if os.path.isfile('point_to_file.csv'):
-            with open('point_to_file.csv', 'a') as f:
+        if os.path.isfile("point_to_file.csv"):
+            with open("point_to_file.csv", "a", encoding="utf-8") as f:
                 f.write(
                     f"\n{point_a[0]},{point_a[1]},{point_b[0]}, {point_b[1]}, "
-                    f"{decimal_point}, {output}, {Error}")
+                    f"{decimal_point}, {output}, {Error}"
+                )
         else:
-            with open('point_to_file.csv', 'w') as f:
+            with open("point_to_file.csv", "w", encoding="utf-8") as f:
                 f.write("X1, Y1, X2, Y2, decimal_point, line_output, Error\n")
                 f.write(
                     f"{point_a[0]},{point_a[1]},{point_b[0]}, {point_b[1]}, "
-                    f"{decimal_point}, {output}, {Error}")
+                    f"{decimal_point}, {output}, {Error}"
+                )
         print(" Results saved to 'point_to_file.csv'")
 
     print(f"{bcolors.OKMSG}*********** Done! ***********{bcolors.ENDC}")
--- point_to_line.py
+++ point_to_line.py
@@ -7,9 +7,8 @@ def point_to_line(point_a: list, point_b: list, decimal_point=3) -> (str, str):
         assert isinstance(point_a, list) and len(point_a) == 2
         assert isinstance(point_b, list) and len(point_b) == 2
         assert isinstance(decimal_point, int) and decimal_point >= 0
-    except AssertionError as err:
-        print(
-            f"{bcolors.FAIL}*********** Error Detected ***********{bcolors.ENDC}")
+    except AssertionError:
+        print(f"{bcolors.FAIL}*********** Error Detected ***********{bcolors.ENDC}")
         print("Input types Error!")
         return None, "Input types Error!"
     try:
@@ -21,37 +20,37 @@ def point_to_line(point_a: list, point_b: list, decimal_point=3) -> (str, str):
             raise ValueError
 
     except TypeError as err:
-        print(
-            f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}"
-        )
+        print(f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}")
         print("Make sure your inputs are numerical!")
         print(f"Your error is: {err}.")
         return None, "Make sure your inputs are numerical!"
     except AssertionError as err:
-        print(
-            f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}"
-        )
+        print(f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}")
         print("Yours inputs for should be less than Infinity!")
         print(f"Your error is: {err}!")
         return None, "Yours inputs for should be less than Infinity!"
     except ValueError as err:
-        print(
-            f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}"
-        )
+        print(f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}")
         print("Please input two different points!")
         print(f"Your error is: {err}!")
         return None, "Please input two different points!"
 
     else:
         if var_a == 0:
-            outline = f"{var_b:.{decimal_point}f} * Y " \
-                      f"= {var_c:.{decimal_point}f} with A = 0"
+            outline = (
+                f"{var_b:.{decimal_point}f} * Y "
+                f"= {var_c:.{decimal_point}f} with A = 0"
+            )
         elif var_b == 0:
-            outline = f"({var_a:.{decimal_point}f}) * X " \
-                      f"= {var_c:.{decimal_point}f} with B = 0"
+            outline = (
+                f"({var_a:.{decimal_point}f}) * X "
+                f"= {var_c:.{decimal_point}f} with B = 0"
+            )
         else:
-            outline = f"({var_a:.{decimal_point}f}) * X " \
-                      f"+ ({var_b:.{decimal_point}f}) * Y " \
-                      f"= {var_c:.{decimal_point}f}"
+            outline = (
+                f"({var_a:.{decimal_point}f}) * X "
+                f"+ ({var_b:.{decimal_point}f}) * Y "
+                f"= {var_c:.{decimal_point}f}"
+            )
 
         return outline, None
