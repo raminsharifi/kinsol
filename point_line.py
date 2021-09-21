06b031355b689b2f8d73cf30b3da81294340f0b8
Ramin Sharifi <raminsharifi@w134-87-178-032.wireless.uvic.ca>
2021-09-21 10:26:38 -0700

    requirements.txt added

--- point_line.py
+++ point_line.py
@@ -24,24 +24,41 @@ class PointToLine:
         self.__var_a = None
         self.__var_b = None
         self.__var_c = None
-    
+        self.__error = " "
+
     def solve(self) -> (str, str):
-        can_solve, error = self.validate_input()
-        if can_solve and error is None:
+        """
+        This functions calculates the output line of the class.
+        :return:
+        * output: None, in case of an error. otherwise: String
+        * Error: None, when no error is detected, otherwise: String
+        """
+        can_solve = self.validate_input()
+        if can_solve:
             self.__var_a = self.point_a[1] - self.point_b[1]
             self.__var_b = self.point_b[0] - self.point_a[0]
             self.__var_c = self.__var_b * self.point_a[1] + self.__var_a * \
                            self.point_a[0]
         else:
-            return None, error
-        can_output, error = self.after_compute_validation()
-        if can_output and error is None:
+            return None, self.__error
+        # check to see the calculations are right.
+        can_output = self.after_compute_validation()
+        if can_output:
+            # If no error is detected, this will run.
             output = self.to_string()
             return output, None
         else:
-            return None, error
+            # In the case of an error this will run.
+            return None, self.__error
 
     def validate_input(self) -> (bool, str):
+        """
+        Checks to see if the inputs are of type list.
+        They should be numbers with the length of two.
+        :return:
+        bool.
+        True: When no error is detected. otherwise, False.
+        """
         try:
             # Checks the inputs to if they are as expected
             assert isinstance(self.point_a, list) and len(
@@ -62,11 +79,19 @@ class PointToLine:
             print(
                 f"{bcolors.FAIL}*********** Error Detected ***********{bcolors.ENDC}")
             print("Input types Error!")
-            return None, "Input types Error!"
+            self.__error = "Input types Error!"
+            return None
         else:
             return True, None
 
-    def after_compute_validation(self) -> (bool, str):
+    def after_compute_validation(self) -> (bool):
+        """
+        after_compute_validation checks to see computed variables are not inf
+        or both A and B are zero.
+        :return:
+        boolean:
+        True: When no error is detected. otherwise, False.
+        """
         try:
             assert math.isinf(self.__var_a) is not True and math.isinf(
                 self.__var_b) is not True
@@ -77,16 +102,24 @@ class PointToLine:
                 f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}")
             print("Yours inputs for should be less than Infinity!")
             print(f"Your error is: {err}!")
-            return None, "Yours inputs for should be less than Infinity!"
+            self.__error = "Yours inputs for should be less than Infinity!"
+            return False
         except ValueError:
             print(
                 f"{bcolors.FAIL}******** Error Detected ********{bcolors.ENDC}")
             print("Please input two different points!")
-            return None, "Please input two different points!"
+            self.__error = "Please input two different points!"
+            return False
         else:
-            return True, None
+            return True
 
     def to_string(self) -> str:
+        """
+        This function will turn the computed variables into the output String.
+        :return:
+        String
+
+        """
         if self.__var_a == 0:
             output = (
                 f"{self.__var_b:.{self.decimal_point}f} * Y "
